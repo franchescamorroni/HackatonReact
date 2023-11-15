ab904564f5f1c569f8f7982921f77218038c1e75
LizettePerez <lizette.perezcorrea@gmail.com>
2023-11-14 22:26:52 -0300

    tabla

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
-import "./index.css";
 import Home from "./pages/Home";
+import "tailwindcss/tailwind.css";
+import "./index.css";
+
 
 
 function App() {
