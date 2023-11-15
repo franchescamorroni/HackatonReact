04fe14386f52b01e979ab4be533d0cad116baaa9
LizettePerez <lizette.perezcorrea@gmail.com>
2023-11-15 04:05:29 -0300

    axios query productos

--- vite-project/src/components/Tabla.jsx
+++ vite-project/src/components/Tabla.jsx
@@ -1,56 +1,90 @@
 import "../index.css";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid } from "@mui/x-data-grid";
 import { useState, useEffect } from "react";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
 import axios from "axios";
 
-
 const columns = [
-  { field: 'id', headerName: 'RUT/RUN', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
-  { field: 'nombre', headerName: 'Nombre', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
-  { field: 'banco', headerName: 'Banco', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
-  { field: 'cuenta', headerName: 'N°Cuenta', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
-  { field: 'monto', headerName: 'Monto($)', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
-  { field: 'producto', headerName: 'Producto', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
-  { field: 'servicio', headerName: 'Código Servicio', disableColumnMenu: true, flex: 1, headerClassName: 'custom-header' },
+  { field: "id", headerName: "RUT/RUN", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
+  { field: "nombre", headerName: "Nombre", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
+  { field: "banco", headerName: "Banco", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
+  { field: "cuenta", headerName: "N°Cuenta", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
+  { field: "monto", headerName: "Monto ($)", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
+  { field: "producto", headerName: "Producto", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
+  { field: "servicio", headerName: "Código Servicio", disableColumnMenu: true, flex: 1, headerClassName: "custom-header" },
 ];
 
 const customLocaleText = {
-  footerRowSelected: (count) => `${count} asdsadsdsd`,
+  footerRowSelected: (count) => `Total seleccionado: ${count} asdsadsdsd`,
 };
 
 export default function Tabla() {
+  const [selectedProductos, setSelectedProductos] = useState([]);
+
+  const handleCheckboxChange = (event) => {
+    const productName = event.target.name;
+    setSelectedProductos((prevSelected) => {
+      if (prevSelected.includes(productName)) {
+        return prevSelected.filter((selected) => selected !== productName);
+      } else {
+        return [...prevSelected, productName];
+      }
+    });
+  };
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/servicio/lista')
-      .then((res) => {
-        // Mapear datos para que coincidan con las columnas del DataGrid
-        const mappedData = res.data.map((row) => ({
-          id: row.rut, // Utilizar el campo rut como identificador único
+    const fetchData = async () => {
+      try {
+        let response;
+
+        if (selectedProductos.length === 2) {
+          // Si ambos checkboxes están marcados, realizar la consulta por defecto
+          response = await axios.get("http://localhost:8080/servicio/lista");
+        } else if (selectedProductos.length > 0) {
+          // Si hay productos seleccionados, realizar la consulta con los productos seleccionados
+          response = await axios.get("http://localhost:8080/servicio/listaPorProducto", {
+            params: { nombreProducto: selectedProductos.join(",") },
+          });
+        } else {
+          // Si no hay productos seleccionados, realizar la consulta sin especificar productos
+          response = await axios.get("http://localhost:8080/servicio/lista");
+        }
+
+        const mappedData = response.data.map((row) => ({
+          id: row.rut,
           rut: row.rut,
           nombre: row.nombreCliente,
           banco: row.nombreBanco,
           cuenta: row.idCuenta,
-          monto: row.monto.toLocaleString('es-CL', { style: 'currency', currency: 'CLP' }),
+          monto: row.monto.toLocaleString("es-CL", { style: "currency", currency: "CLP" }),
           producto: row.nombreProducto,
           servicio: row.idServicio,
         }));
         setData(mappedData);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error("Error al obtener datos:", error);
-      });
-  }, []);
+      }
+    };
 
+    fetchData();
+  }, [selectedProductos]);
 
   return (
-    <div style={{ height: 400, width: '80%', margin: 'auto' }}>
+    <div style={{ height: 400, width: "80%", margin: "auto" }}>
+      <FormGroup>
+        <FormControlLabel control={<Checkbox onChange={handleCheckboxChange} name="APV" />} label="APV" />
+        <FormControlLabel control={<Checkbox onChange={handleCheckboxChange} name="Mis Metas" />} label="Mis Metas" />
+      </FormGroup>
       <DataGrid
         rows={data}
         columns={columns}
         autoHeight
-        getRowId={(row) => row.id} // Utiliza el campo 'id' como identificador de fila
+        getRowId={(row) => row.id}
         initialState={{
           pagination: {
             paginationModel: { page: 0, pageSize: 10 },
